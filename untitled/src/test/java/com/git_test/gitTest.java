3fcd7f246f52323c97abecb62bc97087e490e033
Ty <tan.com>
2022-10-27 22:42:56 +0800

    push test

--- untitled/src/test/java/com/git_test/gitTest.java
+++ untitled/src/test/java/com/git_test/gitTest.java
@@ -6,6 +6,7 @@ public class gitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("master code");
+        System.out.println("push test");
     }
 
 
